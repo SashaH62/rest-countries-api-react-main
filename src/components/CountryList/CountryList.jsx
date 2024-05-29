4dd473cdc818b994cbcc87619b5e908f23b166cd
Sasha Herman <sashahermanwebdev@gmail.com>
2024-05-29 23:24:28 +0200

    Add error for no countries found

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -6,11 +6,22 @@ function CountryList() {
   const { filteredCountries } = useCountries();
 
   return (
-    <div className={styles.countryList}>
-      {filteredCountries.map((country) => (
-        <CountryItem country={country} key={`${country.name.common}-card`} />
-      ))}
-    </div>
+    <>
+      {filteredCountries.length > 0 ? (
+        <div className={styles.countryList}>
+          {filteredCountries.map((country) => (
+            <CountryItem
+              country={country}
+              key={`${country.name.common}-card`}
+            />
+          ))}
+        </div>
+      ) : (
+        <h3 className={styles.noResults}>
+          😱 Looks like there are no countries that match that description.{" "}
+        </h3>
+      )}
+    </>
   );
 }
 
